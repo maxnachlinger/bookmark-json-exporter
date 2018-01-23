2059ca1f956a90a8de063a2a82999f0ca2b6fbd1
Max Nachlinger <mnachlinger@walmartlabs.com>
2018-01-23 07:52:33 -0800

    Split out utils

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { downloadBlob, getCurrentDateStr, jsonStringToBlob } from './util'
+
 // cross browser bookmarks
 window.browser = (() => window.msBrowser || window.browser || window.chrome)()
 
@@ -6,30 +8,6 @@ const getBookmarksJson = (callback) => {
     return callback(new Error('No bookmarks API found.'))
   }
   return window.browser.bookmarks.getTree((nodes) => callback(null, JSON.stringify(nodes, null, 2)))
-}
-
-const jsonStringToBlob = (str) => new Blob([str], { type: 'application/json' })
-
-const zeroPadLessThan10 = (i) => i < 10 ? `0${i}` : i
-
-const getCurrentDateStr = () => {
-  const today = new Date()
-  const day = zeroPadLessThan10(today.getDate())
-  const month = zeroPadLessThan10(today.getMonth() + 1)
-  return `${today.getFullYear()}-${month}-${day}`
-}
-
-const downloadBlob = (filename, blob) => {
-  const url = URL.createObjectURL(blob)
-
-  // all this to get a nicer name for the downloaded file
-  const a = document.createElement('a')
-  document.body.appendChild(a)
-  a.style = 'display: none'
-  a.href = url
-  a.download = filename
-  a.click()
-  window.URL.revokeObjectURL(url)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
---
+++ src/util.js
@@ -0,0 +1,23 @@
+export const downloadBlob = (filename, blob) => {
+  const url = URL.createObjectURL(blob)
+
+  // all this to get a nicer name for the downloaded file
+  const a = document.createElement('a')
+  document.body.appendChild(a)
+  a.style = 'display: none'
+  a.href = url
+  a.download = filename
+  a.click()
+  window.URL.revokeObjectURL(url)
+}
+
+const zeroPadLessThan10 = (i) => i < 10 ? `0${i}` : i
+
+export const getCurrentDateStr = () => {
+  const today = new Date()
+  const day = zeroPadLessThan10(today.getDate())
+  const month = zeroPadLessThan10(today.getMonth() + 1)
+  return `${today.getFullYear()}-${month}-${day}`
+}
+
+export const jsonStringToBlob = (str) => new Blob([str], { type: 'application/json' })
