0740dbd2a05cf8a68aa8a68cd831d54dfe8a24cc
Max Nachlinger <max.nachlinger@gmail.com>
2018-01-22 23:29:31 -0800

    Preparing to support Edge and Firefox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-const getBookmarksJson = (callback) => chrome.bookmarks.getTree((nodes) => callback(JSON.stringify(nodes, null, 2)))
+const getBookmarksApi = () => (chrome || browser || {}).bookmarks
+
+const getBookmarksJson = (callback) => {
+  const api = getBookmarksApi()
+  if (!api) {
+    return callback(new Error('No bookmarks API found.'))
+  }
+  return api.getTree((nodes) => callback(null, JSON.stringify(nodes, null, 2)))
+}
 
 const jsonStringToBlob = (str) => new Blob([str], {type: 'application/json'})
 
-const zeroPadLessThan10 = (i) => i < 10 ? `0${i}` : i;
+const zeroPadLessThan10 = (i) => i < 10 ? `0${i}` : i
 
 const getCurrentDateStr = () => {
-  const today = new Date();
+  const today = new Date()
   const day = zeroPadLessThan10(today.getDate())
   const month = zeroPadLessThan10(today.getMonth() + 1)
   return `${today.getFullYear()}-${month}-${day}`
@@ -25,7 +33,12 @@ const downloadBlob = (filename, blob) => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  return getBookmarksJson((json) => {
+  return getBookmarksJson((error, json) => {
+    if (error) {
+      document.body.innerText = error.message
+      return
+    }
+
     const filename = `bookmarks-export-${getCurrentDateStr()}.json`
     return downloadBlob(filename, jsonStringToBlob(json))
   })
